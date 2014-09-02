b5366a6478ac29b684be4c612566a85855ee7dc0
ccoenraets <ccoenraets@gmail.com>
2014-09-02 16:28:58 -0400

    adding messageId to config.js

--- merges/ios/js/config.js
+++ merges/ios/js/config.js
@@ -4,6 +4,9 @@ angular.module('nibs.config', [])
 
     .constant('HOST','http://nibs-chatter.herokuapp.com' + '/')
 
+    // .constant('ET_MESSAGE_ID', 'MToxMTQ6MA') //dev
+    .constant('ET_MESSAGE_ID', 'MzoxMTQ6MA') //prod
+
     // SSO Initialization URL from the defined Auth Provider
     .constant('CHATTER_SSO', 'https://login.salesforce.com/services/auth/sso/00Di0000000k8FREAY/Nibs')
 
